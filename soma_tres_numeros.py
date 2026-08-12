e167c6008d49b7906ea58f8815d7bfb3f04a1095
João Victor Paixão Oliveira <joaovictorpaixaooliveira80@gmail.com>
2026-08-12 08:33:32 -0300

    Create soma_tres_numeros.py

---
+++ soma_tres_numeros.py
@@ -0,0 +1,5 @@
+Primeiro_número = float(input("Digite o Primeiro número: "))
+Segundo_número = float(input("Digite o Segundo número: "))
+Terceiro_número = float(input("Digite o Terceiro número:"))
+
+print(f"A Soma dos treis valores é: {Primeiro_número + Segundo_número + Terceiro_número}")
