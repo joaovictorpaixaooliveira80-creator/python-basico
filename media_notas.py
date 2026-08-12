22161c641da4791aa6527b02004193dacd0f0371
João Victor Paixão Oliveira <joaovictorpaixaooliveira80@gmail.com>
2026-08-12 20:42:42 -0300

    Create media_notas.py

---
+++ media_notas.py
@@ -0,0 +1,5 @@
+nota_1 = float(input("Digite o valor da primeira nota:"))
+nota_2 = float(input("Digite o valor da segunda nota:"))
+nota_3 = float(input("Digite o valor da terceira nota:"))
+
+print(f" A média do aluno é:{(nota_1+nota_2+nota_3) / 3} ")
