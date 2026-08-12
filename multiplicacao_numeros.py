239917bc5430d726afbca408a5223d64863d09b4
João Victor Paixão Oliveira <joaovictorpaixaooliveira80@gmail.com>
2026-08-12 08:35:49 -0300

    Create multiplicacao_numeros.py

---
+++ multiplicacao_numeros.py
@@ -0,0 +1,4 @@
+Primeiro_número = float(input("Digite o Primeiro_número: "))
+Segundo_número = float(input("Digite o Segundo_número: "))
+
+print(f" A multiplicação dos dois valores é: {Primeiro_número * Segundo_número}")
