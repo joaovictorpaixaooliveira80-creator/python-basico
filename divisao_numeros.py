9b56368d403fb80175fd5f152267de91ae02f219
João Victor Paixão Oliveira <joaovictorpaixaooliveira80@gmail.com>
2026-08-12 19:55:08 -0300

    Add division functionality in divisao_numeros.py

---
+++ divisao_numeros.py
@@ -0,0 +1,4 @@
+numerador = float(input("Digite o numerador: "))
+denominador = float(input("Digite o denominador (não pode ser 0) "))
+
+print(f" A Divisão dos dois números é: {numerador / denominador}")
