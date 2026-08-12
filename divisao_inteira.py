bea53904992b4483e28d17a8f3add276f64779fc
João Victor Paixão Oliveira <joaovictorpaixaooliveira80@gmail.com>
2026-08-12 20:19:20 -0300

    Create divisao_inteira.py

---
+++ divisao_inteira.py
@@ -0,0 +1,4 @@
+numerador = int(input("Digite o numerador: "))
+denominador = int(input("Digite o denominador: (não pode ser 0) "))
+
+print(f" O resultado da divisão é: {numerador // denominador} ")
