5661dcaf528ba5b453b38c2d7ef699e588786f01
João Victor Paixão Oliveira <joaovictorpaixaooliveira80@gmail.com>
2026-08-12 08:31:54 -0300

    Create soma_numeros.py

---
+++ soma_numeros.py
@@ -0,0 +1,4 @@
+Primeiro_número = float(input("Digite o primeiro_número: "))
+Segundo_número = float(input("Digite o segundo_número: "))
+
+print(f"A soma dos dois valores é: {Primeiro_número + Segundo_número}")
