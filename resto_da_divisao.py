e109287157cc72228d82b656c1f992990bf69bb8
João Victor Paixão Oliveira <joaovictorpaixaooliveira80@gmail.com>
2026-08-12 20:31:30 -0300

    Add division remainder calculation script

---
+++ resto_da_divisao.py
@@ -0,0 +1,4 @@
+numerador = float(input("Digite o numerador: "))
+denominador = float(input("Digite o denominador: (não pode ser 0) "))
+
+print(f" O resto da divisão é: {numerador % denominador} ")
