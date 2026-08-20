40bd9f949a976445402aa64cee6d38db9fc16ac0
João Victor Paixão Oliveira <joaovictorpaixaooliveira80@gmail.com>
2026-08-20 20:53:50 -0300

    Add script to find the larger of two numbers

---
+++ maior_numero.py
@@ -0,0 +1,9 @@
+numero_1 = float(input('Digite o primeiro numero: '))
+numero_2 = float(input('Digite o segundo numero: '))
+
+if numero_1 > numero_2:
+  print(f' O maio numero é o {numero_1}')
+elif numero_2 > numero_1:
+  print(f' O maior numero é o {numero_2}')
+else:
+  print("Os dois números são iguais!") 
