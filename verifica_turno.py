e5d76b4fc63ec8e6844daec6abf873bfcb559036
João Victor Paixão Oliveira <joaovictorpaixaooliveira80@gmail.com>
2026-08-24 20:50:07 -0300

    Create verifica_turno.py

---
+++ verifica_turno.py
@@ -0,0 +1,13 @@
+turno = input('Em qual turno você estuda (manhã, tarde ou noite)? ')
+
+if turno == 'manhã':
+  print('Bom Dia')
+
+elif turno == 'tarde':
+  print(' Boa Tarde')
+
+elif turno == 'noite':
+  print('Boa noite')
+
+else:
+  print('Valor invalido')
