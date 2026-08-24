4550dcf7af4b5a91f0f088bbbec3522ee460bf7e
João Victor Paixão Oliveira <joaovictorpaixaooliveira80@gmail.com>
2026-08-24 12:42:17 -0300

    Clean up comments and keep growth check
    Removed initial comments and retained the growth check logic.

---
+++ #analise_percentual.py
@@ -0,0 +1,10 @@
+percentual = float(input('Digite o percentual'))
+
+if percentual > 0:
+  print(f'crescimento de {percentual}%')
+
+elif percentual < 0:
+  print(f'decrescimento de {percentual}%')
+
+else:
+  print(f'não houve crescimento e nem decrescimo {percentual}%')
